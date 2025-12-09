bbb31fb5bedd897c85bd1d8afbba2dbfd8d64960
Khanalashim <khanalashim11@gmail.com>
2025-12-09 13:18:04 +0545

    services color changes, about image issue solve

--- resources/views/frontend/about/index.blade.php
+++ resources/views/frontend/about/index.blade.php
@@ -142,33 +142,30 @@
                 ];
             @endphp
             <div class="row">
-                <div class="col-12">
-                    <div class="why-choose__item-wrapper why-choose__item-two-wrapper d-grid justify-content-between mt-60 mt-md-50 mt-sm-40 mt-xs-30 wow fadeInUp"
-                        data-wow-delay=".7s">
-                        @foreach ($services as $key => $service)
-                            <div class="why-choose__item why-choose__item-two"
-                                style="background-image: url(assets/img/home/why-choose__item-two-overly.png);">
-                                <div class="icon mb-30 mb-lg-20 mb-md-10 mb-xs-5 color-red">
+                    <div class="col-12">
+                        <div class="why-choose__item-wrapper why-choose__item-two-wrapper d-grid justify-content-between mt-60 mt-md-50 mt-sm-40 mt-xs-30 wow fadeInUp"
+                            data-wow-delay=".7s">
+                            @foreach ($services as $key => $service)
+                                <div class="why-choose__item why-choose__item-two"
+                                    style="background-image: url(assets/img/home/why-choose__item-two-overly.png);">
+                                    <div class="icon mb-30 mb-lg-20 mb-md-10 mb-xs-5 color-red">
+                                        {{-- {!! $icons[$key % count($icons)] !!} --}}
+                                        <img height="230px" src="{{ $service->image }}">
+                                    </div>
 
+                                    <h6 class="title color-pd_black fw-600 mb-15 mb-xs-10">{{ $service->title }}</h6>
 
-                                    {{-- {!! $icons[$key % count($icons)] !!} --}}
-                                    <i class="{{ $service->image }}"></i>
+                                    <div class="description font-la mb-20 mb-sm-15 mb-xs-10  line-clamp-4 service-des">
+                                        <p>{!! $service->description !!}</p>
+                                    </div>
 
+                                    <a href="{{ route('frontend.servicesingle', $service->slug) }}"
+                                        class="color-red d-block">Read More <i class="far fa-chevron-double-right"></i></a>
                                 </div>
-
-                                <h6 class="title color-pd_black fw-600 mb-15 mb-xs-10">{{ $service->title }}</h6>
-
-                                <div class="description font-la mb-20 mb-sm-15 mb-xs-10  line-clamp-4 service-des">
-                                    <p>{!! $service->description !!}</p>
-                                </div>
-
-                                <a href="{{ route('frontend.servicesingle', $service->slug) }}"
-                                    class="color-red d-block">Read More <i class="far fa-chevron-double-right"></i></a>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
         </div>
         </div>
     </section>
