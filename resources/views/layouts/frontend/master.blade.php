aa07fa73d48416b2e58113245dba88476d816d90
aasmitadhakal <dhakalreshma52@gmail.com>
2025-12-15 09:42:27 +0545

    gallery edtide

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -1,66 +1,55 @@
 <!DOCTYPE html>
-<html lang="zxx">
+<html lang="en">
 <head>
-    <!-- ========== Meta Tags ========== -->
+    <!-- Meta -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="paradise">
-    <!-- ======== Page title ============ -->
+
+    {{-- SEO --}}
     @yield('seo')
-    <!-- ========== Favicon Icon ========== -->
+
+    {{-- Favicon --}}
     <link rel="icon" type="image/x-icon"
-    href="{{ $settings['site_fav_icon'] ? asset($settings['site_fav_icon']) : 'Umi Advisor' }}" />
-    <script src="{{ asset('admin/assets/js/sweetalert-new.js') }}"></script>
-    <!-- ===========  All Stylesheet ================= -->
+          href="{{ $settings['site_fav_icon'] ? asset($settings['site_fav_icon']) : '' }}">
+
+    {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('assets/css/icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/slick.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"
-        media="print" onload="this.media='all'">
-        <link
-    href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css"
-    rel="stylesheet"
-/>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-{{-- 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
+    {{-- FancyBox CSS (IMPORTANT) --}}
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css">
 </head>
+
 <body class="body-wrapper">
+
+    {{-- Header --}}
     @include('layouts.frontend.header')
+
+    {{-- Page Content --}}
     @yield('content')
 
-    <div class="gtranslate_wrapper"></div>
-    <script>
-        window.gtranslateSettings = {
-            "default_language": "en",
-            "languages": ["en", "ne", "ja"],
-            "wrapper_selector": ".gtranslate_wrapper",
-            "switcher_horizontal_position": "right",
-            "flag_style": "3d"
-        }
-    </script>
-    
-    <script src="
-    https://cdn.gtranslate.net/widgets/latest/float.js"
-    defer></script>
-
-    <a href="https://api.whatsapp.com/send?phone={{ preg_replace('/\D+/', '', $setting['whatsapp_number'] ?? '817092770229') }}" 
-   class="float" 
-   target="_blank">
-    <i class="fa fa-whatsapp my-float"></i>
-</a>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    {{-- Footer --}}
     @include('layouts.frontend.footer')
-    <!--  ALl JS Plugins
-        ====================================== -->
+
+    {{-- Floating WhatsApp --}}
+    <a href="https://api.whatsapp.com/send?phone={{ preg_replace('/\D+/', '', $setting['whatsapp_number'] ?? '') }}"
+       class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
+
+    {{-- JS --}}
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.easing.js') }}"></script>
@@ -68,17 +57,22 @@
     <script src="{{ asset('assets/js/scrollUp.min.js') }}"></script>
     <script src="{{ asset('assets/js/counterup.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.sticky-kit.js') }}"></script>
-    <script src="{{ asset('assets/js/magnific-popup.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.easypiechart.min.js') }}"></script>
     <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
-    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('assets/js/wow.min.js') }}"></cript>
     <script src="{{ asset('assets/js/active.js') }}"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
-    <script src="{{ asset('admin/assets/js/sweetalert-new.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
+    {{-- FancyBox JS (IMPORTANT) --}}
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+
+    {{-- SweetAlert --}}
+    <script src="{{ asset('admin/assets/js/sweetalert-new.js') }}"></script>
+
+    {{-- 🔥 REQUIRED FOR FancyBox & Page JS --}}
+    @stack('js')
 
 </body>
-
 </html>
